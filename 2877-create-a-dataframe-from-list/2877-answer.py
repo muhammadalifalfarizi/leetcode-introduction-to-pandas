b788f64aec21d47e540d3d393372d80bbb747af8
muhammadalifafz <muhammadalifafz@gmail.com>
2024-01-26 21:41:27 +0700

    initialize

---
+++ 2877-create-a-dataframe-from-list/2877-answer.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from typing import List
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    columns = ["student_id", "age"]
+
+    df = pd.DataFrame(student_data, columns=columns)
+    return df
+
+student_data = [
+    [1, 15],
+    [2, 11],
+    [3, 11],
+    [4, 20]
+]
+
+result_df = createDataframe(student_data)
+print(result_df)
+
+
+# Short Answer
+# def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+#     return pd.DataFrame(student_data, columns=["student_id", "age"])
---
+++ 2878-get-the-size-of-a-dataframe/2878-answer.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from typing import List
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
+
+data = {
+    'player_id': [846, 749, 155, 583, 388, 883, 355, 247, 761, 642],
+    'name': ['Mason', 'Riley', 'Bob', 'Isabela', 'Zachary', 'Ava', 'Violet', 'Thomas', 'Jack', 'Charlie'],
+    'age': [21, 30, 28, 32, 24, 23, 18, 27, 33, 36],
+    'position': ['Forward', 'Winger', 'Striker', 'Goalkeeper', 'Midfielder', 'Defender', 'Striker', 'Striker', 'Midfielder', 'Center-back'],
+    'team': ['Real Madrid', 'Barcelona', 'Manchester United', 'Liverpool', 'Bayern Munich',
+             'Chelsea', 'Juventus', 'Paris Saint-Germain', 'Manchester City', 'Arsenal']
+}
+
+players_df = pd.DataFrame(data)
+result = getDataframeSize(players_df)
+print(result)

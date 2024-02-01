37a796a332787c1649569e0427db61547e519670
muhammadalifafz <muhammadalifafz@gmail.com>
2024-02-01 08:13:48 +0700

    added the answer for the question no 2883

---
+++ 2883-drop-missing-data/2883-answer.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    result = students.replace('None', pd.NA).dropna(subset=['name'])
+    return result
+
+students = pd.DataFrame({
+    'student_id' : [32, 217, 779, 849],
+    'name' : ['Piper', 'None', 'Georgia', 'Willow'],
+    'age' : [5, 19, 20, 14]
+})
+
+result_df = dropMissingData(students)
+print(result_df)
+
+'''
+Short Answer
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.dropna(subset=['name])
+'''
+

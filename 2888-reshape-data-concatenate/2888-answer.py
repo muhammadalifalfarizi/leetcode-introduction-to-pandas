77de6321ce36a0cf3f9aa14378c26549c0a08d08
muhammadalifafz <muhammadalifafz@gmail.com>
2024-02-20 23:02:31 +0700

    new question

---
+++ 2888-reshape-data-concatenate/2888-answer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
+
+df1 = pd.DataFrame({
+    'student_id': [1, 2, 3, 4],
+    'name': ['Mason', 'Ava', 'Taylor', 'Georgia'],
+    'age' : [8, 6, 15, 17]
+})
+
+df2 = pd.DataFrame({
+    'student_id' : [5, 6],
+    'name' : ['Leo', 'Alex'],
+    'age' : [7, 7]
+})
+
+result_df = concatenateTables(df1, df2)
+print(result_df)
+
+
+'''
+Short Answer
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1, df2])
+'''

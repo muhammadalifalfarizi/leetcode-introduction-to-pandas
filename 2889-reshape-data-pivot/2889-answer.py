42025e1152cfe4b713374e4e11c596f387b0ac6a
muhammadalifafz <muhammadalifafz@gmail.com>
2024-02-21 21:48:51 +0700

    added new question

--- 2889-reshape-data-pivot/2889-answer.py
+++ 2889-reshape-data-pivot/2889-answer.py
@@ -2,3 +2,18 @@ import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
     return weather.pivot(index='month', columns='city', values='temperature')
+
+weather_data = pd.DataFrame({
+    'city' : ['Jacksonville', 'Jacksonville', 'Jacksonville', 'Jacksonville', 'Jacksonville', 'El Paso', 'El Paso', 'El Paso', 'El Paso', 'El Paso'],
+    'month' : ['January', 'February','March', 'April', 'May', 'January', 'February', 'March','April', 'May'],
+    'temperature' : [13, 23, 38, 5, 34, 20, 6, 26, 2, 43]
+})
+
+result = pivotTable(weather_data)
+print(result)
+
+'''
+Short Answer
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    return weather.pivot(index='month', columns='city', values='temperature')
+'''

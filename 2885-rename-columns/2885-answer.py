c781129d22a11ddf419223320d155382ccf62f6a
muhammadalifafz <muhammadalifafz@gmail.com>
2024-07-13 21:04:36 +0700

    fix the code

--- 2885-rename-columns/2885-answer.py
+++ 2885-rename-columns/2885-answer.py
@@ -2,14 +2,11 @@ import pandas as pd
 
 
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
-    students = students.rename(
-        columns={
+    students = students.rename(columns={
             'id': 'student_id',
             'first': 'first_name',
             'last': 'last_name',
-            'age': 'age_in_years'
-        }
-    )
+            'age': 'age_in_years'})
     return students
 
 
